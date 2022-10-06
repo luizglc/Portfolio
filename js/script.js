a74e84c38b486d36f69af6c55e1c92a6d69277ac
Luiz Gustavo de Lima <luizgustavo.lc01@gmail.com>
2022-10-06 09:30:29 -0300

    att contats and projects

--- js/script.js
+++ js/script.js
@@ -18,3 +18,35 @@ typewriter = () => {
 window.addEventListener('load', typewriter)
 
 
+
+const apiGit = document.querySelector('.projects-container')
+
+function getApiGitHub() {
+  fetch('https://api.github.com/users/luizglc/repos')
+    .then(async res => {
+
+      if (!res.ok) {
+        throw new Error(res.status)
+      }
+
+      var data = await res.json()
+
+      data.map(item => {
+        let div = document.createElement('div')
+        div.className = 'col-md-4 proj-1'
+
+        div.innerHTML = `
+        <strong>${item.name.toUpperCase()}</strong><br>
+        <a class="btn gitButtons" href="${item.html_url}" target="_blank">Ver projeto</a>
+        
+      `
+        apiGit.appendChild(div)
+
+      })
+
+    }).catch(e => console.log(e))
+}
+
+getApiGitHub()
+
+
